ca833103b5ae232e9bd9e9ef797fdea4f7d87ea9
Thimmaraju <thimmaraju418@gmail.com>
2024-01-09 08:14:20 +0530

    completed us4321

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
 
-    "specPattern": "**/*.feature",
+    //"specPattern": "**/*.feature",
 
     projectId: "ovhqr9",
    // "chromeWebSecurity": false,
